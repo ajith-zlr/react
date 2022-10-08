666618ce60564cb50b7adcdaacd709c540c7fe9f
v.ajith <ajith@zelarsoft.com>
2022-10-08 12:10:46 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Greet from './Greet';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>hii</h1>
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/welcome.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+class welcome extends Component{
+    render(){
+        return<h1>classs</h1>
+    }
+}
+export default App;
